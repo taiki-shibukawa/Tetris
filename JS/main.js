8f7a458a52d3246efdf6ec2a34cfeb855a2c18c9
taiki <tai0521ki@gmail.com>
2021-10-01 22:31:04 +0900

    コンポーネントで再利用可能なパーツを作成

--- JS/main.js
+++ JS/main.js
@@ -54,3 +54,21 @@ var app5 = new Vue({
         }
     }
 });
+
+Vue.component('alert-box', {
+    template: `
+    <div class="alert" v-on:click="caution">
+        <strong>Error!</strong>
+        <slot></slot>
+    </div>
+    `,
+    methods: {
+        caution: function() {
+            alert('クリックされました');
+        }
+    }
+});
+
+var app = new Vue({
+    el: "#app6"
+});
